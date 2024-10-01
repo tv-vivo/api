c6c226d841efe235d105725d1a71bdb2a2546cce
tv-vivo <botandresvpn@gmail.com>
2024-10-01 14:09:53 -0500

    Update modulos.js

--- modulos.js
+++ modulos.js
@@ -6,8 +6,6 @@ const path = require('path');
 const fs = require("fs")
 const {getBuffer, axios} = require("./config")
 const fetch = require("node-fetch");
-const texpro1 = require("./lib/textpro");
-const TelegraPh = require("./lib/telegra")
 module.exports = {
 express,
 cors,
@@ -16,6 +14,4 @@ fs,
 getBuffer,
 axios,
 fetch,
-texpro1,
-TelegraPh
 }
